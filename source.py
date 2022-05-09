6ea9d5e57e133af108d759721f030d0bb9e232c0
AI_xplore <98645710+AIxploreRCC@users.noreply.github.com>
2022-05-09 19:10:24 +0200

    Update source.py

--- source.py
+++ source.py
@@ -82,3 +82,11 @@ st.write(surv)
 if st.button('Submit'):
     surv2 = model.predict_survival_function(dff, return_array=True)
     st.write(surv2)
+    
+for i, s in enumerate(surv2):
+    plt.step(model.event_times_, s, where="post", label=str(i))
+plt.ylabel("Survival probability")
+plt.xlabel("Time in days")
+plt.legend()
+plt.grid(True)
+    
