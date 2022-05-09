f26e76ed7bc55cce9f0ac0ef1f2bc59de8dfda76
AI_xplore <98645710+AIxploreRCC@users.noreply.github.com>
2022-05-09 20:21:22 +0200

    Update source.py

--- source.py
+++ source.py
@@ -86,5 +86,9 @@ event_times_ = np.arange(1, 120)
 
 st.write(surv2)
 
-for s in surv2:
-    plt.step(event_times_, s (event_times_), where="post")
+for s in enumerate(surv2):
+    plt.step(model.event_times_, s, where="post")
+plt.ylabel("Survival probability")
+plt.xlabel("Time in days")
+plt.legend()
+plt.grid(True)
