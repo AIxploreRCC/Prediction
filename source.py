20d01b4bc0bc63e93abe0699bd707c6ab8645f03
AI_xplore <98645710+AIxploreRCC@users.noreply.github.com>
2022-05-10 20:14:51 +0200

    Update source.py

--- source.py
+++ source.py
@@ -54,10 +54,7 @@ CHOICES = {0: 'No', 1: 'Yes'}
 def format_func_yn(option):
     return CHOICES[option]
   
- 
-CHOICES2 = {1: 'RCC', 2: 'pRCC', 3: 'chRCC', 4: 'other'}
-def format_func_yn(option):
-    return CHOICES2[option]
+
   
 
 # get inputs
